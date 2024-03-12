7c334a47395db6f068fc48d21920e9f34b2d92b1
gun <safocum@gmail.com>
2024-03-12 22:07:04 +0200

    date append 0 to porfoy fix

--- generAlpha.py
+++ generAlpha.py
@@ -84,114 +84,122 @@ for row in range(longest_row -4): #Row -4 because son marjlarin 4 ceyrek oncesi 
         ozkaynak_percentage_change = (data_frame['Özkaynak Karlılığı'] - copy_df['Özkaynak Karlılığı']) /  data_frame['Özkaynak Karlılığı']
         data_frame['Özkaynak Karlılığı Puan'] = ozkaynak_percentage_change.apply(lambda x: 0 if x <= 0 else (1 if x >= 1 else x)).round(5)
 
-        data_frame = data_frame[:-4]
+        # data_frame = data_frame[:-4]
 
-        in_query = get_row_or_zero(data_frame, row)
-        in_query = in_query.to_frame().T
-        in_query.set_index(pd.Index([file_name]), inplace=True)
+        # in_query = get_row_or_zero(data_frame, row)
+        if row in data_frame.index:
+            in_query = data_frame.iloc[row]
+            in_query = in_query.to_frame().T
+            in_query.set_index(pd.Index([file_name]), inplace=True)
+            # show(in_query)
 
-        last_trading_day = '2024/02/15'
-        #Checking Criterias
-        if (in_query['F/K Puan'] > 0.98).any():
-            yfstock = file_name + '.IS'
-            file_buy_date = pd.read_excel('/home/gun/Documents/Amele/RaporTarihleri/{}.xlsx'.format(file_name)).T
-            file_buy_date = file_buy_date[0].iloc[1:]
-            file_sell_date = pd.read_excel('/home/gun/Documents/Amele/RaporTarihleri/{}.xlsx'.format(file_name)).T
+            last_trading_day = '2024/02/15'
+            #Checking Criterias
+            if (in_query['Net Kar Marjı Yıllık'] > 0.1).any():
+                yfstock = file_name + '.IS'
+                file_buy_date = pd.read_excel('/home/gun/Documents/Amele/RaporTarihleri/{}.xlsx'.format(file_name)).T
+                file_buy_date = file_buy_date[0].iloc[1:]
+                file_sell_date = pd.read_excel('/home/gun/Documents/Amele/RaporTarihleri/{}.xlsx'.format(file_name)).T
 
-            if row in file_buy_date.index: #Checking If Buy Date Exists In File
-                raw_date = file_buy_date[row]
-                raw_date = datetime.strptime(raw_date, '%Y/%m/%d').strftime('%Y-%m-%d')
-                date = datetime.strptime(raw_date, '%Y-%m-%d')
-                buy_date = date + timedelta(days=1)
-                
-                file_sell_date[0].iloc[0] = last_trading_day
-                raw_sell_date = file_sell_date[0].iloc[row]
-                raw_sell_date = datetime.strptime(raw_sell_date, '%Y/%m/%d').strftime('%Y-%m-%d')
-                sell_date = datetime.strptime(raw_sell_date, '%Y-%m-%d')
-                sell_date = sell_date + timedelta(days=1)
+                if row in file_buy_date.index: #Checking If Buy Date Exists In File
+                    raw_date = file_buy_date[row]
+                    raw_date = datetime.strptime(raw_date, '%Y/%m/%d').strftime('%Y-%m-%d')
+                    date = datetime.strptime(raw_date, '%Y-%m-%d')
+                    buy_date = date + timedelta(days=1)
+                    
+                    file_sell_date[0].iloc[0] = last_trading_day
+                    raw_sell_date = file_sell_date[0].iloc[row]
+                    raw_sell_date = datetime.strptime(raw_sell_date, '%Y/%m/%d').strftime('%Y-%m-%d')
+                    sell_date = datetime.strptime(raw_sell_date, '%Y-%m-%d')
+                    sell_date = sell_date + timedelta(days=1)
 
-                #Checking if the buy date after the report release is weekend or not and adjust buy date
-                while buy_date.weekday() >= 5:
-                    days_until_monday = (7 - buy_date.weekday()) % 7
-                    buy_date += timedelta(days=days_until_monday)
-                buy_date_str = buy_date.strftime('%Y-%m-%d')
+                    #Checking if the buy date after the report release is weekend or not and adjust buy date
+                    while buy_date.weekday() >= 5:
+                        days_until_monday = (7 - buy_date.weekday()) % 7
+                        buy_date += timedelta(days=days_until_monday)
+                    buy_date_str = buy_date.strftime('%Y-%m-%d')
 
-                #Checking if the sell date after the report release is weekend or not and adjust sell date
-                while sell_date.weekday() >= 5:
-                    days_until_monday2 = (7 - sell_date.weekday()) % 7
-                    sell_date += timedelta(days=days_until_monday2)
+                    #Checking if the sell date after the report release is weekend or not and adjust sell date
+                    while sell_date.weekday() >= 5:
+                        days_until_monday2 = (7 - sell_date.weekday()) % 7
+                        sell_date += timedelta(days=days_until_monday2)
 
-                data = yf.download(yfstock, start=buy_date_str)['Close']
+                    data = yf.download(yfstock, start=buy_date_str)['Close']
 
-                #Adjusting Buy Date According To YF_Dates If Buy Date Is Abnormal Holiday
-                buy_day_difference = (data.index[0] - buy_date).days
+                    #Adjusting Buy Date According To YF_Dates If Buy Date Is Abnormal Holiday
+                    buy_day_difference = (data.index[0] - buy_date).days
 
-                if buy_date in data.index:
-                    buy_date = buy_date.strftime('%Y-%m-%d')
-                    buy_price = round(data.iloc[0], 2)
+                    if buy_date in data.index:
+                        buy_date = buy_date.strftime('%Y-%m-%d')
+                        buy_price = round(data.iloc[0], 2)
 
-                #Get The Closest Trading Date in YF, limited to 10 days to prevent loop for the earliest date in the file
-                elif 0 <= buy_day_difference <= 10:
-                    buy_date = data.index[0]
-                    buy_date = buy_date.strftime('%Y-%m-%d')
-                    buy_price = round(data.iloc[0], 2)
+                    #Get The Closest Trading Date in YF, limited to 10 days to prevent loop for the earliest date in the file
+                    elif 0 <= buy_day_difference <= 10:
+                        buy_date = data.index[0]
+                        buy_date = buy_date.strftime('%Y-%m-%d')
+                        buy_price = round(data.iloc[0], 2)
+
+                    else:
+                        buy_date = buy_date.strftime('%Y-%m-%d')
+                        buy_price = 0
+                    
+                    #Adjusting Sell Date According To YF_Dates If Sell Date Is Abnormal Holiday
+                    closest_index = min(filter(lambda date: date > sell_date, data.index))
+                    sell_day_difference = (closest_index - sell_date).days
+                    closest_index = datetime.strftime(closest_index, '%Y-%m-%d')
+
+                    if sell_date in data.index: #Checking If YF_Data Has Older Date
+                        sell_price = round(data[sell_date], 2)
+                        sell_date = sell_date.strftime('%Y-%m-%d')
+
+                    #Get The Closest Trading Date in YF, limited to 10 days to prevent loop for the earliest date in the file
+                    elif 0 < sell_day_difference <= 10:
+                        sell_price = round(data[closest_index], 2)
+                        sell_date = closest_index
+
+                    else:
+                        sell_price = 0
+                        sell_date = closest_index
+
+                    # print(yfstock)
+                    # print(buy_date)
+                    # print(buy_price)
+                    # print(sell_date)
+                    # print(sell_price)
+
+                    if buy_price and sell_price != 0:
+                        position_pnl = ((sell_price - buy_price) / buy_price) * 100
+                        position_pnl = round(position_pnl, 5)
+
+                    elif buy_price == 0:
+                        position_pnl = 0
+
+                    else:
+                        position_pnl = 0
+
+                    in_query['Alış Tarihi'] = buy_date
+                    in_query['Alış Fiyatı'] = buy_price
+                    in_query['Satış Tarihi'] = sell_date
+                    in_query['Satış Fiyatı'] = sell_price
+                    in_query['Kar/Zarar'] = position_pnl
 
                 else:
-                    buy_date = buy_date.strftime('%Y-%m-%d')
-                    buy_price = 0
-                
-                #Adjusting Sell Date According To YF_Dates If Sell Date Is Abnormal Holiday
-                closest_index = min(filter(lambda date: date > sell_date, data.index))
-                sell_day_difference = (closest_index - sell_date).days
-                closest_index = datetime.strftime(closest_index, '%Y-%m-%d')
-
-                if sell_date in data.index: #Checking If YF_Data Has Older Date
-                    sell_price = round(data[sell_date], 2)
-                    sell_date = sell_date.strftime('%Y-%m-%d')
-
-                #Get The Closest Trading Date in YF, limited to 10 days to prevent loop for the earliest date in the file
-                elif 0 < sell_day_difference <= 10:
-                    sell_price = round(data[closest_index], 2)
-                    sell_date = closest_index
-
-                else:
-                    sell_price = 0
-                    sell_date = closest_index
-
-                # print(yfstock)
-                # print(buy_date)
-                # print(buy_price)
-                # print(sell_date)
-                # print(sell_price)
-
-                if buy_price and sell_price != 0:
-                    position_pnl = ((sell_price - buy_price) / buy_price) * 100
-                    position_pnl = round(position_pnl, 5)
-
-                elif buy_price == 0:
                     position_pnl = 0
-
-                else:
-                    position_pnl = 0
-
-                in_query['Alış Tarihi'] = buy_date
-                in_query['Alış Fiyatı'] = buy_price
-                in_query['Satış Tarihi'] = sell_date
-                in_query['Satış Fiyatı'] = sell_price
-                in_query['Kar/Zarar'] = position_pnl
+                
+                portfollio = portfollio._append(in_query)
 
             else:
-                position_pnl = 0
-            
-            portfollio = portfollio._append(in_query)
+                continue
 
-        else:
-            continue
+            portfollio = portfollio.sort_values(by='Alış Tarihi', ascending=True)
 
-        portfollio = portfollio.sort_values(by='Alış Tarihi', ascending=True)
+            if len(portfollio) > 10:
+                portfollio = portfollio[:10]
 
-        if len(portfollio) > 10:
-            portfollio = portfollio[:10]
+            else:
+                continue
+
+            # output.append(portfollio)
 
         else:
             continue
@@ -200,5 +208,5 @@ for row in range(longest_row -4): #Row -4 because son marjlarin 4 ceyrek oncesi 
     # show(portfollio)
     # break
 df = pd.concat(output, axis=0)
-df.to_excel('/home/gun/Documents/Siralamalar/DefaultAgirlik/BilancoToBilanco/SingleRatioSorts/F_K098.xlsx', sheet_name="FK Only", index=True)
+df.to_excel('/home/gun/Documents/Siralamalar/DefaultAgirlik/BilancoToBilanco/SingleRatioSorts/NKY.xlsx', sheet_name="NKY Only", index=True)
 # show(df)
